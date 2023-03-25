335c07f2f8fee5bdf8827bc0e9e92c325afcfe05
by <2582928157@qq.com>
2023-03-25 16:41:41 +0800

    second commit

--- src/main/java/by_git_demo/git_demo_test.java
+++ src/main/java/by_git_demo/git_demo_test.java
@@ -3,5 +3,7 @@ package by_git_demo;
 public class git_demo_test {
     public static void main(String[] args) {
         System.out.println("hello,git!");
+        System.out.println("hello,git2!");
+        
     }
 }
