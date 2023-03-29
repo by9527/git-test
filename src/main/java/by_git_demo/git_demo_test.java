633caea20fa8c99deaa80161fb813d72dd7d2f0a
by <2582928157@qq.com>
2023-03-29 14:11:20 +0800

    gitee commit

--- src/main/java/by_git_demo/git_demo_test.java
+++ src/main/java/by_git_demo/git_demo_test.java
@@ -12,6 +12,8 @@ public class git_demo_test {
         System.out.println("hot测试提交是否push成功");
         System.out.println("再次测试hot测试提交是否push成功");
         System.out.println("第一次测试hot测试提交是否pull成功");
+        System.out.println("第二次测试码云push是否成功");
+        System.out.println("第三次测试码云push是否成功");
 
     }
 }
